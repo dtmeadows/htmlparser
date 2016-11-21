5eb4e4e19336dcf9a019bc03462afc931d41e191
David Meadows <dmeadows@stripe.com>
2016-11-21 14:32:58 -0800

    Grabs all tagged text now and stores in array

--- parse.rb
+++ parse.rb
@@ -2,6 +2,15 @@
 require 'nokogiri'
 require 'pry'
 
-page = Nokogiri::HTML(open("2fa.htm"))
+page = Nokogiri::HTML(open("dev.htm"))
 
+tags = page.css(".started")
+
+days= Array.new
+
+tags.each do |tag| 
+	days << tag.text.strip
+end
+
+binding.pry
 
